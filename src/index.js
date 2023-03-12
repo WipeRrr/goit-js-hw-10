e9ee74e68728cac4bdcd3ab4cd3928efd5dcd159
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-03-12 13:21:49 +0200

    package inst

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
+
+Notiflix.Loading.hourglass();
+console.log(fetch(`
+https://restcountries.com/v3.1/name/peru`));
