252fe2ee6c535160726bf672c1238108f4521ff9
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-03-18 00:57:59 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import fetchCountries from './js/fetchCountries';
+import fetchCountries from './js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 
